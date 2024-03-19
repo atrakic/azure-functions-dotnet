25c69824a4c99db748bfd1a932bb3b2c049e99b7
Admir <xomodo@gmail.com>
2024-03-19 15:12:52 +0100

    [ci skip]: Update

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -5,13 +5,21 @@
 : "${STORAGE_NAME:?'You need to configure the STORAGE_NAME variable!'}"
 : "${LOCATION:=westeurope}"
 
+set -x
 set -o errexit
 #set -o nounset
 #set -o pipefail
 
+premiumPlan="$APP_NAME-PremiumPlan"
+skuStorage="Standard_LRS" # Allowed values: Standard_LRS, Standard_GRS, Standard_RAGRS, Standard_ZRS, Premium_LRS, Premium_ZRS, Standard_GZRS, Standard_RAGZRS
+functionsVersion="4"
+skuPlan="EP1"
+
+
+# shellcheck disable=SC2046
 if [ $(az group exists --name "$RG") = false ]; then
 
-  echo "You are about to create Azure resources at account: "
+  echo "You are about to create Azure resources on account: "
   az account show
 
   if [[ -n "${FORCE}" ]]; then
@@ -21,6 +29,7 @@ if [ $(az group exists --name "$RG") = false ]; then
 
   ## https://docs.microsoft.com/azure/azure-resource-manager/resource-group-create-service-principal-portal
   if [[ -n "${CREATE_AZURE_SP}" ]]; then
+    # shellcheck disable=SC2219
     let "randomIdentifier=$RANDOM*$RANDOM"
     servicePrincipalName="$APP_NAME-$randomIdentifier"
     subscriptionId=$(az account show --query id --output tsv)
@@ -33,14 +42,33 @@ if [ $(az group exists --name "$RG") = false ]; then
   az group create --name "$RG" --location "$LOCATION"
 
   az storage account create --name "$STORAGE_NAME" \
-    --sku Standard_LRS --allow-blob-public-access false \
+    --sku "$skuStorage" \
+    --allow-blob-public-access false \
     --resource-group "$RG" --location "$LOCATION"
 
-  az functionapp create --name "$APP_NAME" \
-    --resource-group "$RG" \
-    --consumption-plan-location "$LOCATION" \
-    --runtime dotnet-isolated --functions-version 4 \
-    --storage-account "$STORAGE_NAME"
+  if [ -n "${PREMIUM_PLAN}" ]; then
+
+    az functionapp plan create \
+      --name "$premiumPlan" \
+      --sku "$skuPlan" \
+      --resource-group "$RG" --location "$LOCATION"
+
+    az functionapp create --name "$APP_NAME" \
+      --runtime dotnet-isolated \
+      --plan "$premiumPlan" \
+      --functions-version "$functionsVersion" \
+      --storage-account "$STORAGE_NAME" \
+      --resource-group "$RG"
+
+  else
+
+    az functionapp create --name "$APP_NAME" \
+      --runtime dotnet-isolated \
+      --functions-version "$functionsVersion" \
+      --storage-account "$STORAGE_NAME" \
+      --resource-group "$RG" \
+      --consumption-plan-location "$LOCATION"
+  fi
 
   if [[ -n "${COSMOSDB_NAME}" ]]; then
     az cosmosdb create --name "$COSMOSDB_NAME" \
@@ -51,5 +79,3 @@ if [ $(az group exists --name "$RG") = false ]; then
 else
   echo "Resource group: $RG already exists"
 fi
-
-## WEBSITE_RUN_FROM_PACKAGE
